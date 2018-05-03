79e3ae8e521939736c4d71792b38ecdbeec2adbf
herny14 <35711203+herny14@users.noreply.github.com>
2018-05-03 10:21:54 +0700

    Update static method

--- src/com/amiabledata/archertracker/score/Score.java
+++ src/com/amiabledata/archertracker/score/Score.java
@@ -1,14 +1,42 @@
 package com.amiabledata.archertracker.score;
 
-public abstract class Score {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.*;
+public class Score {
     private Integer SetNumber;
-
-    public void setSetNumber(Integer value) {
+    
+    public void setSetNumber(Integer value){
         this.SetNumber = value;
     }
-
-    public Integer getSetNumber() {
+    public Integer getSetNumber(){
         return this.SetNumber;
+    }
+    public static Integer getStaticSum(ArrayList<Integer> value) {
+    	Integer sum = 0;
+    	for(Iterator<Integer>it = value.iterator(); it.hasNext();) {
+    		sum+=it.next();
+    	}
+    	return sum;
+    }
+     public static Integer getStaticSum(Integer[] value) {
+    	Integer sum = 0;
+    	for(Integer i = 0; i < value.length; i++) {
+    		sum+=value[i];
+    	}
+    	return sum;
+    }
+     public static Integer getStaticSum(Integer[][] value) {
+     Integer sum = 0;
+     for(Integer i = 0; i < value.length; i++) {
+      for(Integer j = 0; j < value[i].length;j++){
+         sum+=value[i][j];
+      }
+     }
+     return sum;
     }
 
 }
+
+    
+   
